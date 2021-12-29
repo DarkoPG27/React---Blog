b7e1b36ac26e9b9b854cbb187bf4560f00fbd1fa
Darko <89515851+DarkoPG27@users.noreply.github.com>
2021-12-29 23:46:26 +0100

    Making a POST request

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,21 @@ const Create = () => {
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
     const [author, setAuthor] = useState("John");
+    const [isLoading, setIsLoading] = useState(false);
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = { title, body, author };
+        setIsLoading(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            setIsLoading(false)
+        })
     }
 
     return (
@@ -17,7 +27,7 @@ const Create = () => {
             <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input
-                    type=""
+                    type="text"
                     required
                     value={title}
                     onChange={(e) => setTitle(e.target.value)}
@@ -37,7 +47,8 @@ const Create = () => {
                     <option value="John">John</option>
                     <option value="Wick">Wick</option>
                 </select>
-                <button>Add Blog</button>
+                {!isLoading && <button>Add Blog</button>}
+                {isLoading && <button disabled>Adding Blog...</button>}
             </form>
         </div>
     );
