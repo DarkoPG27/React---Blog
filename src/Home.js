10e9e67d5f493c671f62aab99cde0ab1be504427
Darko <89515851+DarkoPG27@users.noreply.github.com>
2021-12-29 10:38:05 +0100

    Added some blogs and blog styles

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: "My first website", body: "lorem ipsum...", author: "John", id: 1 },
+        { title: "My second website", body: "lorem ipsum...", author: "Wick", id: 2 },
+        { title: "My website", body: "lorem ipsum...", author: "John Wick", id: 3 }
+    ]);
+
     return (
         <div className="home">
-            <h2>Homepage </h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+                </div>
+            ))}
+
         </div>
     );
 }
