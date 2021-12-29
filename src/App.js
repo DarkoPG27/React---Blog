580d5cc555573a5578458df4ebb3eb59b4868714
Darko <89515851+DarkoPG27@users.noreply.github.com>
2021-12-29 09:15:10 +0100

    Added Home and Navbar components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+
   return (
     <div className="App">
-      App Page
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
