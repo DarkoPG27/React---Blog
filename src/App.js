4cae5a3cc7fb55c9e3c08d7938627d61e34d2134
Darko <89515851+DarkoPG27@users.noreply.github.com>
2021-12-30 00:09:07 +0100

    Completed project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
+import NotFound from './NotFound';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import BlogDetails from './BlogDetalis';
 
@@ -16,6 +17,9 @@ function App() {
             <Route exact path="/" > <Home /> </Route>
             <Route path="/create" > <Create /> </Route>
             <Route path="/blogs/:id" > <BlogDetails /> </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
           </Switch>
         </div>
       </div>
