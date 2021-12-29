db0912a6db8e50de6497e004d6d711a577a630f0
Darko <89515851+DarkoPG27@users.noreply.github.com>
2021-12-29 11:13:22 +0100

    Aded Delete_Blog Buttton and handleDelete

--- src/Home.js
+++ src/Home.js
@@ -8,9 +8,18 @@ const Home = () => {
         { title: "My website", body: "lorem ipsum...", author: "John Wick", id: 3 }
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs);
+    }
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} />
+            <BlogList
+                blogs={blogs}
+                title="All Blogs!"
+                handleDelete={handleDelete}
+            />
         </div>
     );
 }
